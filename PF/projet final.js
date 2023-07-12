e2e890446a853c07b4d10f5ce4c2253f1a2cb910
abdullahdir <135703647+abdullahdir@users.noreply.github.com>
2023-07-12 05:00:20 -0400

    Add files via upload

---
+++ PF/projet final.js
@@ -0,0 +1,55 @@
+const question = [
+    {
+        question:"ana win?",
+        answers: [
+            
+               { Text:"tunis",correct:true } 
+               { Text:"usa",correct:false } 
+               { Text:"french",correct:false } 
+               { Text:"brazil",correct:false } 
+            
+        ]
+         
+    },
+    {
+        question:"ana chkon?"
+        answers: [
+            
+               { Text:"aymen",correct:false } 
+               { Text:"ahmed",correct:false } 
+               { Text:"abdullah",correct:true } 
+               { Text:"adem",correct:false } 
+            
+        ]
+
+    },
+    {
+        question:"3asimt tons"
+        answers: [
+            
+               { Text:"touns",correct:true } 
+               { Text:"shark blue",correct:false } 
+               { Text:"5ortom",correct:false } 
+               { Text:"dog",correct:false } 
+            
+        ]
+
+    },
+    {
+        question:"makilt touns"
+        answers: [
+            
+               { Text:"makarouna",correct:true } 
+               { Text:"shark blue",correct:false } 
+               { Text:"shark jc",correct:false } 
+               { Text:"shark djcjd",correct:false } 
+            
+        ]
+    }
+      
+
+
+
+
+
+
